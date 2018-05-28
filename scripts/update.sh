c5a3e9f3018ba8583a770fb4a37813e88ad9d04e
Dirk Malorny <dirk.malorny@ligatus.com>
2018-05-28 08:13:20 +0200

    update status only if ssid has changed

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,7 @@
 ROOT=$HOME/Library/SlackLoc
 source $ROOT/config.sh
 
+LAST_SSID=$(cat $ROOT/last_ssid)
 SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}')
 line=$(grep ^"$SSID" $ROOT/known_ssids)
 
@@ -14,8 +15,15 @@ else
   TEXT=$DEFAULT_STATUS
 fi
 
+if [ "$LAST_SSID" = "$SSID" ]
+then
+  echo "No update necessary".
+  exit 0
+fi
+
 STATUS=$(echo "{\"status_text\":\"$TEXT\",\"status_emoji\":\"$EMOJI\"}" | $ROOT/urlencode.sh)
 for i in "${TOKENS[@]}"
 do
   curl -d "token=$i&profile=$STATUS" https://slack.com/api/users.profile.set
 done
+echo $SSID >$ROOT/last_ssid
